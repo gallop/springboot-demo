90057ea1493bf9e6113becdbbf41d8c72de201c0
gallop <39100782@qq.com>
2021-03-26 14:17:59 +0800

    project init

---
+++ src/main/java/com/gallop/springboot/ComGallopSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.gallop.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComGallopSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComGallopSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gallop/springboot/ComGallopSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gallop.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComGallopSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
